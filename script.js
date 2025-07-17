456d0f75ae92c275851aae291e318426168b4ee3
Arti4758 <147397489+Arti4758@users.noreply.github.com>
2025-07-17 10:00:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const display = document.getElementById("display");
 
-function appendValue(value) {
-  display.value += value;
+function append(val) {
+  display.value += val;
 }
 
 function clearDisplay() {
@@ -12,10 +12,25 @@ function deleteLast() {
   display.value = display.value.slice(0, -1);
 }
 
-function calculateResult() {
+function calculate() {
   try {
     display.value = eval(display.value);
-  } catch (e) {
+  } catch {
     display.value = "Error";
   }
 }
+
+// Keyboard support
+document.addEventListener("keydown", function (e) {
+  const key = e.key;
+
+  if ("0123456789.+-*/".includes(key)) {
+    append(key);
+  } else if (key === "Enter") {
+    calculate();
+  } else if (key === "Backspace") {
+    deleteLast();
+  } else if (key === "Escape") {
+    clearDisplay();
+  }
+});
